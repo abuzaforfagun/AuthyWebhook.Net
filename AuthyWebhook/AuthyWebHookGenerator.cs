659c3e79c03575ce03174e4d2ba69558f184b696
Fagun <mdabuzaforfagun@gmail.com>
2019-04-26 01:16:38 +0600

    Add and use RequestModel

--- AuthyWebhook/AuthyWebHookGenerator.cs
+++ AuthyWebhook/AuthyWebHookGenerator.cs
@@ -26,14 +26,15 @@ namespace AuthyWebhook
         {
 
             string method = "POST";
-            string sortedParams =
+            var sortedParams =
                 $"access_key={configuration.AccessKey}&app_api_key={configuration.ApiKey}&events%5B%5D={webHookConfiguration.EventName}&name={webHookConfiguration.Name}&url={Uri.EscapeDataString(webHookConfiguration.CallBackUrl)}";
 
             string dataToSign = $"{nonce}|{method}|{_url}|{sortedParams}";
 
-            var computed_sig = cryptographyHelper.GenerateHmac(dataToSign, configuration.SigninKey);
+            var hmacSignature = cryptographyHelper.GenerateHmac(dataToSign, configuration.SigninKey);
             
-            return await SendHttpRequest(webHookConfiguration.CallBackUrl, webHookConfiguration.Name, webHookConfiguration.EventName, computed_sig);
+            var requestModel = new RequestModel(webHookConfiguration, hmacSignature);
+            return await SendHttpRequest(requestModel);
 
         }
 
@@ -42,20 +43,20 @@ namespace AuthyWebhook
             return CreateWebhooksAsync(webHookConfiguration).GetAwaiter().GetResult();
         }
 
-        private async Task<string> SendHttpRequest(string callBackUrl, string name, string events, string computed_sig)
+        private async Task<string> SendHttpRequest(RequestModel request)
         {
             HttpClient client = new HttpClient();
             string response = "";
             var requestContent = new FormUrlEncodedContent(new[] {
-                new KeyValuePair<string, string>("url", callBackUrl),
-                new KeyValuePair<string, string>("name", name),
-                new KeyValuePair<string, string>("events[]", events),
+                new KeyValuePair<string, string>("url", request.CallbackUrl),
+                new KeyValuePair<string, string>("name", request.Name),
+                new KeyValuePair<string, string>("events[]", request.EventName),
                 new KeyValuePair<string, string>("app_api_key", configuration.ApiKey),
                 new KeyValuePair<string, string>("access_key", configuration.AccessKey),
             });
 
             client.DefaultRequestHeaders.Add("X-Authy-Signature-Nonce", nonce);
-            client.DefaultRequestHeaders.Add("X-Authy-Signature", computed_sig);
+            client.DefaultRequestHeaders.Add("X-Authy-Signature", request.HmacSignature);
             try
             {
                 var result = await client.PostAsync(_url, requestContent);
---
+++ AuthyWebhook/Models/RequestModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthyWebhook.Models
+{
+    public class RequestModel
+    {
+        public string CallbackUrl { get; }
+        public string Name { get; }
+        public string EventName { get; }
+        public string HmacSignature { get; }
+
+        public RequestModel(WebHookConfiguration configuration, string hmacSignature)
+        {
+            Name = configuration.Name;
+            EventName = configuration.EventName;
+            HmacSignature = hmacSignature;
+            CallbackUrl = configuration.CallBackUrl;
+        }
+
+        public RequestModel()
+        {
+            
+        }
+    }
+}
