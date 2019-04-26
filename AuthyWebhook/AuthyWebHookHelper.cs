fbc17c6d198848ca9d2b018f6ef595d6e36f4f9e
Fagun <mdabuzaforfagun@gmail.com>
2019-04-27 02:19:42 +0600

    Add comment for intellij suggestion

--- AuthyWebhook/AuthyWebHookHelper.cs
+++ AuthyWebhook/AuthyWebHookHelper.cs
@@ -21,6 +21,12 @@ namespace AuthyWebhook
             cryptographyHelper = new CryptographyHelper();
         }
 
+        /// <summary>
+        /// Create new Webhook in asyc way
+        /// </summary>
+        /// <typeparam name="T">Response or string</typeparam>
+        /// <param name="webHook">Webhook details</param>
+        /// <returns>Newly created webhook in T format</returns>
         public async Task<T> CreateAsync<T>(WebHook webHook)
         {
             string dataToSign = GetDataToSign(webHook);
@@ -32,6 +38,11 @@ namespace AuthyWebhook
             return await authyClient.SendHttpRequest<T>(requestModel);
         }
 
+        /// <summary>
+        /// Get all registered webhooks in asyc way
+        /// </summary>
+        /// <typeparam name="T">IList<Response> or string</typeparam>
+        /// <returns>List of registered webhooks in T format</returns>
         public async Task<T> GetAsync<T>()
         {
             var webHook = new WebHook();
@@ -44,6 +55,11 @@ namespace AuthyWebhook
             return await authyClient.SendHttpRequest<T>(requestModel);
         }
 
+        /// <summary>
+        /// Delete already exist webhook in asyc way
+        /// </summary>
+        /// <param name="webHookId">Webhook Id what you want to delete</param>
+        /// <returns>Action result</returns>
         public async Task<bool> DeleteAsync(string webHookId)
         {
             var webHook = new WebHook(webHookId);
@@ -67,31 +83,61 @@ namespace AuthyWebhook
             return $"{nonce}|{webHook.RequestType}|{Constants.GetAuthyUrl(webHook.Id)}|{sortedParams}"; ;
         }
 
+        /// <summary>
+        /// Create new Webhook
+        /// </summary>
+        /// <typeparam name="T">Response or string</typeparam>
+        /// <param name="webHook">Webhook details</param>
+        /// <returns>Newly created webhook in T format</returns>
         public T Create<T>(WebHook webHook)
         {
             return CreateAsync<T>(webHook).GetAwaiter().GetResult();
         }
 
+        /// <summary>
+        /// Create new Webhook in asyc way
+        /// </summary>
+        /// <param name="webHook">Webhook details</param>
+        /// <returns>Newly created webhook in string format</returns>
         public async Task<string> CreateAsync(WebHook webHook)
         {
             return await CreateAsync<string>(webHook);
         }
 
+        /// <summary>
+        /// Create new Webhook
+        /// </summary>
+        /// <param name="webHook">Webhook details</param>
+        /// <returns>Newly created webhook in string format</returns>
         public string Create(WebHook webHook)
         {
             return Create<string>(webHook);
         }
 
+        /// <summary>
+        /// Get all registered webhooks
+        /// </summary>
+        /// <typeparam name="T">IList<Response> or string</typeparam>
+        /// <returns>List of registered webhooks in T format</returns>
         public T Get<T>()
         {
             return GetAsync<T>().GetAwaiter().GetResult();
         }
 
+        /// <summary>
+        /// Get all registered webhooks
+        /// </summary>
+        /// <returns>List of registered webhooks in string format</returns>
         public string Get()
         {
             return GetAsync<string>().GetAwaiter().GetResult();
         }
 
+        /// <summary>
+        /// Delete already exist webhook
+        /// </summary>
+        /// <param name="webHookId">Webhook Id what you want to delete</param>
+        /// <returns>Action result</returns>
         public bool Delete(string webHookId)
         {
             return DeleteAsync(webHookId).GetAwaiter().GetResult();
